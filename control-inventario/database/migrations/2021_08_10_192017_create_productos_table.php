2decb4a89886d940be2ad71bd17df57a4cf02af4
laurayess <56653712+laurayess@users.noreply.github.com>
2021-08-10 14:43:01 -0500

    Creacion del proyecto

---
+++ control-inventario/database/migrations/2021_08_10_192017_create_productos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->double('precio_v', 8, 2)->nullable();
+            $table->double('precio_c', 8, 2)->nullable();
+            $table->integer('stock')->nullable();
+            $table->boolean('status_delete');
+            $table->string('imagen');
+            $table->ForeignId('id_categoria')->references('id')->on('categorias');
+            $table->ForeignId('id_catalogo')->references('id')->on('catalogos');
+            $table->ForeignId('id_bodega')->references('id')->on('bodegas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ control-inventario/database/migrations/2021_08_10_192035_create_reportes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+            $table->id();
+            $table->string('accion');
+            $table->integer('cantidad');
+            $table->boolean('status_delete');
+            $table->ForeignId('id_usuario')->references('id')->on('usuarios');
+            $table->ForeignId('id_auth')->references('id')->on('usuarios');
+            $table->ForeignId('id_producto')->references('id')->on('productos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reportes');
+    }
+}
