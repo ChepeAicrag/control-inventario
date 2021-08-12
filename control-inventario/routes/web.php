9ee187404924fe8863190a5c5a10d452d93fa43a
@perwin119 <perwin119@gmail.com>
2021-08-12 18:21:41 -0500

    Crud sin diseño

--- control-inventario/routes/web.php
+++ control-inventario/routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//rutas de catalogo
+Route::get('Catalogo','App\Http\Controllers\CatalogoController@index');
+
 Route::get('Crear-catalogo','App\Http\Controllers\CatalogoController@create');
 
 Route::post('Guardar-catalogo','App\Http\Controllers\CatalogoController@store');
@@ -28,3 +31,18 @@ Route::get('Editar-catalogo/{catalogo}','App\Http\Controllers\CatalogoController
 Route::post('Actualizar-catalogo','App\Http\Controllers\CatalogoController@update');
 
 Route::get('Baja-catalogo/{catalogo}','App\Http\Controllers\CatalogoController@destroy');
+
+//rutas de reporte
+Route::get('Reporte','App\Http\Controllers\ReporteController@index');
+
+Route::get('Crear-Reporte','App\Http\Controllers\ReporteController@create');
+
+Route::post('Guardar-Reporte','App\Http\Controllers\ReporteController@store');
+
+Route::get('Mostrar-Reporte','App\Http\Controllers\ReporteController@show');
+
+Route::get('Editar-Reporte/{reporte}','App\Http\Controllers\ReporteController@edit');
+
+Route::post('Actualizar-Reporte','App\Http\Controllers\ReporteController@update');
+
+Route::get('Baja-Reporte/{reporte}','App\Http\Controllers\ReporteController@destroy');
